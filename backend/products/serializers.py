fa0b55a7fea7fe64d10904a9bedb1bd7260c0ea1
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-02 20:27:02 +0200

    Product, Color, Size, Image Serializers with Fields

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,23 +1,59 @@
 from dataclasses import field
 from rest_framework import serializers
-from .models import Color, Product, Size
+from .models import Product, Image, Color, Size
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Product
-        # fields = '__all__'
-        fields = '__all__'
-
-
-class SizeSerializer(serializers.ModelSerializer):
-   class Meta:
-      model = Size
-      fields = '__all__'
+        model = Image
+        fields = ['ImageURL', 'ImageDescription', 'product_id']
 
 
 class ColorSerializer(serializers.ModelSerializer):
-   class Meta:
-      model = Color
-      fields = '__all__'
+
+    class Meta:
+        model = Color
+        fields = ['name', 'product_id']
+
+
+class SizeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Size
+        fields = ['name', 'product_id']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # def APIOverView(self, request):
+    #     api_urls = {
+    #         'All-Items': 'all/',
+    #         'Search by Category': '/?category=category_name/',
+    #         'Search by SubCategory': '/?category=category_name/',
+    #         'Add': '/create/',
+    #         'Update': '/update/pk/',
+    #         'Delete': '/item/pk/delete/',
+
+    #     }
+    #     return Response(api_urls)
+
+    product_images = ImagesSerializer(read_only=True, many=True)
+    product_colors = ColorSerializer(read_only=True, many=True)
+    product_sizes = SizeSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Product
+        ordering = ['publish_date']
+
+        fields = [
+            'title',
+            'description',
+            'quantity',
+            'category',
+            'subcategory',
+            'category',
+            'price',
+            'product_images',
+            'product_colors',
+            'product_sizes',
+        ]
