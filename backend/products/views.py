0a1a51627c58c18eb29a2efd9dd7ecd9678c80c1
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-02 20:26:24 +0200

    Generics List, View APIs CBVs

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Image, Product, Color, Size
+from .serializers import ProductSerializer, ImagesSerializer, ColorSerializer, SizeSerializer
 
 
-class ProductList(generics.ListAPIView):
+class ProductList(generics.ListCreateAPIView):
     """
-    List All Products
+    Home URLs
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    # lookup_field = 'pk'
+
+
+class ProductImage(generics.ListCreateAPIView):
+    """
+    List Or Create New Product Image
+    """
+    queryset = Image.objects.all()
+    serializer_class = ImagesSerializer
+    # lookup_field = 'pk'
+
+
+class ProductColor(generics.ListCreateAPIView):
+    """
+    List Or Create New Product Colors
+    """
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializer
+    # lookup_field = 'pk'
+
+
+class ProductSize(generics.ListCreateAPIView):
+    """
+    List Or Create New Product Sizes
+    """
+    queryset = Size.objects.all()
+    serializer_class = SizeSerializer
     # lookup_field = 'pk'
