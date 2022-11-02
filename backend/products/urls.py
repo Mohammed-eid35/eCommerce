2b6babbb307a6d7c8ce6ec9301eb854eecb946f1
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-02 20:25:57 +0200

    Endpoint URLs For Product Properties

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 
 from .views import (
-    ProductList
+    ProductList, ProductImage, ProductColor, ProductSize
 )
 
 
 urlpatterns = [
-    path('', ProductList.as_view(), name='Products'),
+    # List or create products
+    path('', ProductList.as_view()),
+
+
+    # List or Create Images For Products
+    path('images/', ProductImage.as_view()),
+
+    # List or Create Sizes For Products
+    path('colors/', ProductColor.as_view()),
+
+    # List or Create Colors For Products
+    path('sizes/', ProductSize.as_view()),
+
+
 ]
