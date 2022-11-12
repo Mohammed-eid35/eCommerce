abcd02dec9b5cd3d92b663a025cfb56c5682b887
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2022-11-12 22:16:02 +0200

    logout URL for Rounting creater

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,13 +4,16 @@ from .views import (
     RegisterView,
     LoginAPIView,
     RequestEmailVerificationView,
-    VerifyEmailView
+    VerifyEmailView,
+    LogoutAPIView
 )
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('request-email-verification/', RequestEmailVerificationView.as_view(), name='request-email-verification'),
+    path('request-email-verification/', RequestEmailVerificationView.as_view(),
+         name='request-email-verification'),
     path('email-verify/', VerifyEmailView.as_view(), name="email-verify"),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
