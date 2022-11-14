483ddc839226784db2ce087354d8157c30a1603b
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2022-11-14 15:08:57 +0200

    create a Request password Reset email API view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -9,12 +9,17 @@ import jwt
 from django.conf import settings
 import os
 
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 from .serializers import (
     RegisterSerializer,
     LoginSerializer,
     RequestEmailSerializer,
     EmailVerificationSerializer,
-    LogoutSerializer
+    LogoutSerializer,
+    ResetPasswordEmailRequestSerializer
 )
 from .models import User
 from .utils import Util, CustomRedirect
@@ -115,3 +120,30 @@ class LogoutAPIView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RequestPasswordResetEmailAPIView(generics.GenericAPIView):
+    serializer_class = ResetPasswordEmailRequestSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        email = request.data.get('email', '')
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site = get_current_site(
+                request=request).domain
+            relativeLink = reverse(
+                'password-reset-confirm', kwargs={'uidb64': uidb64, 'token': token})
+
+            redirect_url = request.data.get('redirect_url', '')
+            absurl = 'http://'+current_site + relativeLink
+            email_body = 'Hello, \n Use link below to reset your password  \n' + \
+                absurl+"?redirect_url="+redirect_url
+            data = {'email_body': email_body, 'to_email': user.email,
+                    'email_subject': 'Reset your passsword'}
+            Util.send_email(data)
+        return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
