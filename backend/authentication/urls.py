28d1da8dcea0eb626b41cb024ddf93f4610b9d7a
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2022-11-14 15:17:36 +0200

    create reset password urls for routing

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -5,7 +5,10 @@ from .views import (
     LoginAPIView,
     RequestEmailVerificationView,
     VerifyEmailView,
-    LogoutAPIView
+    LogoutAPIView,
+    PasswordTokenCheckAPIView,
+    SetNewPasswordAPIView,
+    RequestPasswordResetEmailAPIView
 )
 
 
@@ -16,4 +19,10 @@ urlpatterns = [
          name='request-email-verification'),
     path('email-verify/', VerifyEmailView.as_view(), name="email-verify"),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('request-reset-email/', RequestPasswordResetEmailAPIView.as_view(),
+         name="request-reset-email"),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordTokenCheckAPIView.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete')
 ]
