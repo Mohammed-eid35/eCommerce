8f3d629ff017f54b33c1fe7c3723e3f5f880caa4
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2022-11-14 15:11:27 +0200

    create a password token Check API view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -19,7 +19,8 @@ from .serializers import (
     RequestEmailSerializer,
     EmailVerificationSerializer,
     LogoutSerializer,
-    ResetPasswordEmailRequestSerializer
+    ResetPasswordEmailRequestSerializer,
+    SetNewPasswordSerializer
 )
 from .models import User
 from .utils import Util, CustomRedirect
@@ -147,3 +148,34 @@ class RequestPasswordResetEmailAPIView(generics.GenericAPIView):
                     'email_subject': 'Reset your passsword'}
             Util.send_email(data)
         return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
+
+
+class PasswordTokenCheckAPIView(generics.GenericAPIView):
+    serializer_class = SetNewPasswordSerializer
+
+    def get(self, request, uidb64, token):
+
+        redirect_url = request.GET.get('redirect_url')
+
+        try:
+            id = smart_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                if len(redirect_url) > 3:
+                    return CustomRedirect(redirect_url+'?token_valid=False')
+                else:
+                    return CustomRedirect(os.environ.get('FRONTEND_URL', '')+'?token_valid=False')
+
+            if redirect_url and len(redirect_url) > 3:
+                return CustomRedirect(redirect_url+'?token_valid=True&message=Credentials Valid&uidb64='+uidb64+'&token='+token)
+            else:
+                return CustomRedirect(os.environ.get('FRONTEND_URL', '')+'?token_valid=False')
+
+        except DjangoUnicodeDecodeError as identifier:
+            try:
+                if not PasswordResetTokenGenerator().check_token(user):
+                    return CustomRedirect(redirect_url+'?token_valid=False')
+
+            except UnboundLocalError as e:
+                return Response({'error': 'Token is not valid, please request a new one'}, status=status.HTTP_400_BAD_REQUEST)
