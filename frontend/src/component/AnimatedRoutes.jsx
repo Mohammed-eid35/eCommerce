f1c83463791ccf5d1ce8d752a13861c52e68e8be
BelalMedhat <belalmedhat0102@gmail.com>
2022-11-24 21:47:30 +0200

    adding routes animation

---
+++ frontend/src/component/AnimatedRoutes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Home } from "./homePage/Home";
+import { Login } from "./login/Login";
+import { SignUp } from "./signup/signUp";
+import { AnimatePresence } from "framer-motion";
+import { ForgetPassword } from "./forgetPass/forgetPassword";
+import { ResetPassword } from "./forgetPass/resetPass";
+export const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+      </Routes>
+      {/* <ForgetPassword /> */}
+    </AnimatePresence>
+  );
+};
